192cc0d2d456fee84aae25bb32c2d0bc3c54d823
Samriti Bajwa <Samritibajwa@gmail.com>
2022-04-28 11:54:38 -0700

    Big Iversion Assignment

---
+++ src/components/FunctionNewName.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FunctionNewName =(props) => {
+        return(
+            <div>
+
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </div>
+        );
+        }
+export default PersonComponent
